0330a0a5105e96dcf56e8e32b70cc0016bf51b1a
Wang lei <wldandan@gmail.com>
2016-02-04 22:51:59 +0800

    Enable client config

--- src/main/java/com/shangduyuan/services/event/EventServiceApplication.java
+++ src/main/java/com/shangduyuan/services/event/EventServiceApplication.java
@@ -1,10 +1,35 @@
 package com.shangduyuan.services.event;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EventServiceApplication {
+
+    @Value("${database.url}")
+    String dbUrl;
+
+    @Value("${database.username}")
+    String dbUser;
+
+    @Value("${database.password}")
+    String dbPass;
+
+    @Autowired
+    void setEnvironment(Environment e) {
+        System.out.println(e.getProperty("configuration.projectName"));
+    }
+
+    @RequestMapping("/")
+    String hello() {
+        return "DB access by " + dbUrl + ":" + dbUser + ":" + dbPass;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
