66584cc9b7a0a991ce1bd2bba7e2a856ad3fd9cd
Wang lei <wldandan@gmail.com>
2016-02-04 23:51:00 +0800

    Enable refresh configuration dynamically

--- src/main/java/com/shangduyuan/services/event/EventServiceApplication.java
+++ src/main/java/com/shangduyuan/services/event/EventServiceApplication.java
@@ -3,13 +3,17 @@ package com.shangduyuan.services.event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 @RestController
+@RefreshScope // important!
 public class EventServiceApplication {
 
     @Value("${database.url}")
@@ -20,11 +24,6 @@ public class EventServiceApplication {
 
     @Value("${database.password}")
     String dbPass;
-
-    @Autowired
-    void setEnvironment(Environment e) {
-        System.out.println(e.getProperty("configuration.projectName"));
-    }
 
     @RequestMapping("/")
     String hello() {
