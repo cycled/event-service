857372ba922736b0717df3a14529a24ce89326a8
Wang lei <wldandan@gmail.com>
2016-02-05 19:27:49 +0800

    Enable eureka client

--- src/main/java/com/shangduyuan/services/event/EventServiceApplication.java
+++ src/main/java/com/shangduyuan/services/event/EventServiceApplication.java
@@ -4,21 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 public class EventServiceApplication {
 
-    @Value("${database.url}")
+    @Value("${database.url:localhost}")
     String dbUrl;
 
-    @Value("${database.username}")
+    @Value("${database.username:admin}")
     String dbUser;
 
-    @Value("${database.password}")
+    @Value("${database.password:admin}")
     String dbPass;
 
     @Autowired
